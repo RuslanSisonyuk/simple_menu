0024e18cfb18d2b6f66ddfbed8a7d74af496328e
Ruslan <randomrocketgalaxy@gmail.com>
2024-05-01 19:13:27 +0300

    Initial Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#simple menu
+
+def menu():
+	user_input = input("\nEnter option: ")
+	match user_input:
+		case "0":
+			print("Closing menu")
+			return 0
+		case "1":
+			print("You pressed 1!")
+			menu()
+		case "2":
+			print("That's not 1!")
+			menu()
+		case _:
+			print("lmao")
+			menu()
+
+menu()
