805a62d320287323026c43999ff6f6ab9606d99c
Ruslan <randomrocketgalaxy@gmail.com>
2024-06-04 17:36:50 +0300

    Implemented a 'stored opeation' class and Converted the two execute operation functions into one

--- test.py
+++ test.py
@@ -9,11 +9,16 @@
 
 import math
 
+class stored_operation:
+	def __init__(self,values,operation,result):
+		self.values = values
+		self.operation = operation
+		self.result = result
 
 
 def read_one_var():
 	var = float(input("\nNumber: "))
-	return var
+	return var,0
 
 def read_two_vars():
 	var1 = float(input("\nFirst number: "))
@@ -40,29 +45,31 @@ def power2(number):
 	return pow(number,2)
 
 def store_operation(numbers,operation,result):
-	return {numbers,operation,result}
+	op = stored_operation(numbers,operation,result)
+	return op
 
 def view_op_history(op_history):
 	for op in op_history:
-		print(f"\n{op}")
+		if(op.operation == '^'):
+			print(f"\n{op.values[0]} ^ 2 = {op.result}")
+		elif(op.operation == "~"):
+			print(f"\n{op.values[0]} ~= {op.result}")
+		else:
+			print(f"\n{op.values[0]} {op.operation} {op.values[1]} = {op.result}")
 	input("//Continue//")
 
-def execute_option_type1(op_history,func,symbol):
-	numbers = read_two_vars()
-	result = func(numbers)
-	print(f"\n{numbers[0]} {symbol} {numbers[1]} = {result}\n")
-
-	op_history += store_operation(numbers,symbol,result)
-	
+def execute_option(op_history,func,symbol):
+	if(symbol == '^' or symbol == '~'):
+		numbers = read_one_var()
+		result = func(numbers[0])
+		print(f"\n{numbers[0]} {symbol} = {result}\n")
+	else:
+		numbers = read_two_vars()
+		result = func(numbers)
+		print(f"\n{numbers[0]} {symbol} {numbers[1]} = {result}\n")
+	op_history.append(stored_operation(numbers,symbol,result))
 	input("/Back to menu/")
 	menu(op_history)
-
-def execute_option_type2(op_history,func):
-	number = read_one_var()
-	print(f"\nResult of operation for {number} is: {func(number)}")
-	input("/Back to menu/")
-	menu(op_history)
-
 
 def menu(op_history):
 		print("Options:")
@@ -82,20 +89,19 @@ def menu(op_history):
 				print("Closing menu")
 				exit(1)
 			case "1":
-				execute_option_type1(op_history,addition,'+')
+				execute_option(op_history,addition,'+')
 			case "2":
-				execute_option_type1(op_history,subtraction,'-')
+				execute_option(op_history,subtraction,'-')
 			case "3":
-				execute_option_type1(op_history,multiply,'*')
+				execute_option(op_history,multiply,'*')
 			case "4":
-				execute_option_type1(op_history,division,'/')
+				execute_option(op_history,division,'/')
 			case "5":
-				execute_option_type2(op_history,round_number)
+				execute_option(op_history,round_number,'~')
 			case "6":
-				execute_option_type2(op_history,power2)
+				execute_option(op_history,power2,'^')
 			case "7":
-				#view_op_history(op_history)
-				print(op_history)
+				view_op_history(op_history)
 				menu(op_history)
 			case _:
 				menu(op_history)
